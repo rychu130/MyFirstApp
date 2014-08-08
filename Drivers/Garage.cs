29ac32acada9d4dae97ffd5154b6544348155e99
Ryszard Grodzicki <grodzicki.ryszard@gmail.com>
2014-08-08 17:15:32 +0200

    Added Garage class

---
+++ Drivers/Garage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Drivers
+{
+    class Garage
+    {
+        Vehicle[] list;
+        public int numberOfCars { get; set; }
+
+    }
+}
